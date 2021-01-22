fea23b7c1381f24598320e4f574fdcb2f168eade
Assim <assimbuyumad59@gmail.com>
2021-01-22 22:32:26 +0100

    Grass Shader  FIRST PASS + HDRP test scene

---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Wind/WindControlledEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindControlledEntity : MonoBehaviour
+{
+    private void Start()
+    {
+        InitiateWind();
+    }
+
+    [ContextMenu("Refresh Wind")]
+    void InitiateWind()
+    {
+        if(TryGetComponent<Renderer>(out Renderer m_renderer))
+        {
+            if (m_renderer.sharedMaterial != null)
+            {
+                m_renderer.sharedMaterial.SetVector("_WindDirection", WindManager.GlobalWindDirection);
+            }
+           
+        }
+    }
+}
---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Wind/WindManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindManager : MonoBehaviour
+{
+    public static WindManager m_instance;
+    private static Vector4 localWindDir = new Vector4(.5f, 0f,.5f,1f);
+    public static Vector4 GlobalWindDirection 
+    { 
+        get 
+        { 
+            return localWindDir.normalized; 
+        }
+        set
+        {
+            localWindDir = value;
+        }
+        
+    }
+
+    private void Awake()
+    {
+        m_instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

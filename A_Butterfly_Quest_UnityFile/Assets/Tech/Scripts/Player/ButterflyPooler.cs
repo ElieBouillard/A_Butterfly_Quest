ff76ec60a511ca812c4c2d43ad6153dd93301492
Elie667 <elie.bouillard@lacatholille.fr>
2021-01-28 17:39:50 +0100

    [ADD] - Pool Bullets and Shoot

---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Player/ButterflyPooler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyPooler : MonoBehaviour
+{
+    public static ButterflyPooler SharedInstance;
+
+    public List<GameObject> pooledButterfly;
+    public GameObject ButterflyToPool;
+    public int amountToPool;
+
+    private void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    private void Start()
+    {
+        pooledButterfly = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(ButterflyToPool);
+            obj.SetActive(false);
+            pooledButterfly.Add(obj);
+        }
+    }
+
+    //Retourne le premier objet inactif disponible
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledButterfly.Count; i++)
+        {
+            if (!pooledButterfly[i].activeInHierarchy)
+            {
+                return pooledButterfly[i];
+            }
+        }
+        return null;
+    }
+}
--- A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Player/Shoot.cs
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Player/Shoot.cs
@@ -2,66 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using System;
 
 public class Shoot : MonoBehaviour
 {
-    public GameObject PlayerMesh;
-    private CharacterMeshRotate CharacterMeshScpt;
+    [Header("Shooting variables")]
+    [Range(1, 10)]
+    public int Damage = 1;
+    [Range(1, 100)]
+    public float Range = 50;
+    private RaycastHit ShootInfo;
+    public GameObject PrefabButterly;
+    private GameObject ButterflyLauncher;
 
+    [Header("References")]
     public Animator CamAnimator;
-
-    public GameObject CrossAirObj;
-
+    public GameObject CrosshairObj;
     public CinemachineFreeLook freeLookCam;
-    public CameraAiming CameraAimingScpt;    
+    private CameraAiming CameraAimingScpt;
+    private GameObject PlayerMesh;
 
+    [HideInInspector]
     public bool Aiming = false;
 
-    private void Start()
+    private void Awake()
     {
-        CharacterMeshScpt = PlayerMesh.GetComponent<CharacterMeshRotate>();
-    }  
+        CameraAimingScpt = GetComponent<CameraAiming>();
+        PlayerMesh = gameObject.transform.GetChild(0).gameObject;
+        ButterflyLauncher = PlayerMesh.transform.GetChild(0).gameObject;
+    }
 
     private void Update()
     {        
+        //Aim
         if (Input.GetAxis("Aim") > 0)
         {
-
             Aiming = true;
             CamAnimator.SetBool("AimCamera", true);
-            ShowOnCrossair();            
+            ShowCrosshair(true);            
         }
+        //No Aim
         else if (Input.GetAxis("Aim") <= 0)
         {
+            ShowCrosshair(false);
             ResetFreeLookCamPos();
             ResetAimCamPos();
             Aiming = false;
             CamAnimator.SetBool("AimCamera", false);
-            ShowOffCrossair();
+        }
+
+        //Shoot
+        if (Input.GetButtonDown("Fire1") && Aiming)
+        {
+            ShootButterfly();
         }
     }
 
+    private void ShootButterfly()
+    {
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out ShootInfo, Range))
+        {
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * Range, Color.yellow, 10f);
+            GameObject currButterfly = ButterflyPooler.SharedInstance.GetPooledObject();
+            if(currButterfly != null)
+            {
+                currButterfly.transform.position = ButterflyLauncher.transform.position;
+                currButterfly.SetActive(true);
+                ButterFlyBullet currButterFlyScpt = currButterfly.GetComponent<ButterFlyBullet>();
+                currButterFlyScpt.target = ShootInfo.point;
+            }
+
+        }
+    }
+
+    //Reset la camera "Aim" derriere le joueur dès que Aim = false
     public void ResetAimCamPos()
     {
         if (CameraAimingScpt.yAxis.Value != 0)
         {
             CameraAimingScpt.yAxis.Value = 0;
         }
-        //CameraAimingScpt.xAxis.Value = 0;
     }
 
+    //Reset la camera "FreeLook" sur l'orientation de la camera "Aim" dès que Aim = false
     public void ResetFreeLookCamPos()
     {
         freeLookCam.m_XAxis.Value = CameraAimingScpt.xAxis.Value;
     }
 
-    public void ShowOnCrossair()
+    public void ShowCrosshair(bool value)
     {
-        CrossAirObj.SetActive(true);
-    }
-
-    public void ShowOffCrossair()
-    {
-        CrossAirObj.SetActive(false);
+        CrosshairObj.SetActive(value);
     }
 }

2dc162cdf7edd74734bb71971e393742ceb1ff6a
Elie667 <elie.bouillard@lacatholille.fr>
2021-02-09 21:55:03 +0100

    [ADD] - HealthSystem

---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Player/HealthSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [Header("Stats")]
+    public float InitialHealth;
+
+
+    [Header("Debug")]
+    public float CurrHealth;
+    public Material TakingDamageFeedBack;
+
+    private void Start()
+    {
+        CurrHealth = InitialHealth;
+    }
+
+    public void TakeDamage(float DamageValue)
+    {
+        CurrHealth -= DamageValue;
+
+        if(CurrHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Delete()
+    {
+        Destroy(this);
+    }
+}

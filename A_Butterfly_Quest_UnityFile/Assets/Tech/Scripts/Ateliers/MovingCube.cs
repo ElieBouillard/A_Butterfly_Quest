1df8646cbd74ea1895e451867ffbcafb0e5ef2d2
Elie667 <elie.bouillard@lacatholille.fr>
2021-04-02 11:18:05 +0200

    [ADD] - Gizmos Raycast Moving Cube

--- A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Ateliers/MovingCube.cs
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Ateliers/MovingCube.cs
@@ -8,6 +8,15 @@ public class MovingCube : MonoBehaviour
     Vector3 target;
     bool canMoove = true;
     float mooveClock;
+    private Vector3 rayPos;
+
+
+    [Header("Parametres Raycast")]
+    [Range(-1.5f,1.5f)]
+    public float OffsetY;
+    public enum Direction{Front, Left, Back, Right};
+    public Direction RayDir;
+
     private void Start()
     {
         target = new Vector3(transform.position.x, transform.position.y, transform.position.z);
@@ -27,6 +36,7 @@ public class MovingCube : MonoBehaviour
             canMoove = true;
         }
 
+        rayPos = new Vector3(transform.position.x, transform.position.y + OffsetY, transform.position.z);
     }
     private void OnCollisionEnter(Collision hit)
     {
@@ -35,13 +45,35 @@ public class MovingCube : MonoBehaviour
             Character3D playerScpt = hit.gameObject.GetComponent<Character3D>();
             int indexValue = ButterflyTypeSelection.Instance.SelectionTypeValue;
             Vector3 dir = hit.contacts[0].normal;
-            bool raycast = Physics.Raycast(transform.position, dir, 3f);
+            bool raycast = Physics.Raycast(rayPos, dir, 4.49f);
 
             if (playerScpt.canDash == false && indexValue == 2 && canMoove && !raycast)
             {
-                target = new Vector3(transform.position.x, transform.position.y, transform.position.z) + dir *3f;
+                target = new Vector3(transform.position.x, transform.position.y, transform.position.z) + dir * 3f;
                 mooveClock = 2f;
             }           
         }
-    }    
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 rayPosTemp = new Vector3(transform.position.x, transform.position.y + OffsetY, transform.position.z);
+        if (RayDir == Direction.Front)
+        {
+            Gizmos.DrawRay(rayPosTemp, transform.forward * 4.49f);
+        }
+        else if (RayDir == Direction.Left)
+        {
+            Gizmos.DrawRay(rayPosTemp, -transform.right * 4.49f);
+        }
+        else if (RayDir == Direction.Back)
+        {
+            Gizmos.DrawRay(rayPosTemp, -transform.forward * 4.49f);
+        }
+        else if (RayDir == Direction.Right)
+        {
+            Gizmos.DrawRay(rayPosTemp, transform.right * 4.49f);
+        }
+    }
 }

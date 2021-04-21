6dfc666ede71f62b48477a2f2cbd48372721db9f
Elie667 <elie.bouillard@lacatholille.fr>
2021-04-21 16:44:36 +0200

    [ADD] - Prefab Receptacle

---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Player/RespawnSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnSystem : MonoBehaviour
+{
+    public Collider[] TriggerColliderSpawn;
+    public Transform[] RepsawnPointTransform;
+
+    private void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}

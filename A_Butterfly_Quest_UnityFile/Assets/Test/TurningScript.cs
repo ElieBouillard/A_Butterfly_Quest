c5b573cfe7ea730fbc06c058d3bfd8d015654f5e
Elie667 <elie.bouillard@lacatholille.fr>
2021-05-17 23:46:11 +0200

    רובין הכלבלב

---
+++ A_Butterfly_Quest_UnityFile/Assets/Test/TurningScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningScript : MonoBehaviour
+{
+
+    private void Update()
+    {
+        transform.Rotate(0, 2, 0 * Time.deltaTime);
+    }
+}

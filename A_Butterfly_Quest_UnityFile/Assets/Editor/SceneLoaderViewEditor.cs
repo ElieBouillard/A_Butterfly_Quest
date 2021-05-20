12c5369e0812d8f3829276df4c9a700d0a0b4c7d
Robin Leplat <robin.leplat26@gmail.com>
2021-05-20 16:23:03 +0200

    Ajout nouveaux ateliers / regalge clone / Suppr SceneLoader

--- A_Butterfly_Quest_UnityFile/Assets/Editor/SceneLoaderViewEditor.cs
+++ A_Butterfly_Quest_UnityFile/Assets/Editor/SceneLoaderViewEditor.cs
@@ -7,35 +7,35 @@ using UnityEditor;
 [InitializeOnLoad]
 public class SceneLoaderViewEditor : Editor
 {
-    static Rect buttonRect = new Rect(Vector2.zero, new Vector2(150,40));
-    static SceneLoaderViewEditor()
-    {
-        SceneView.duringSceneGui += OnSceneGUI;
-    }
+    //static Rect buttonRect = new Rect(Vector2.zero, new Vector2(150,40));
+    //static SceneLoaderViewEditor()
+    //{
+    //    SceneView.duringSceneGui += OnSceneGUI;
+    //}
 
-    static void OnSceneGUI(SceneView sceneView)
-    {
-        Handles.BeginGUI();
+    //static void OnSceneGUI(SceneView sceneView)
+    //{
+    //    Handles.BeginGUI();
         
-        if (!SceneHierarchyViewer.scenePreviewing)
-        {
+    //    if (!SceneHierarchyViewer.scenePreviewing)
+    //    {
 
-            if(GUI.Button(buttonRect,"Preview All Scenes"))
-            {
-                SceneLoader.PreviewScenes();
-            }
+    //        if(GUI.Button(buttonRect,"Preview All Scenes"))
+    //        {
+    //            SceneLoader.PreviewScenes();
+    //        }
            
-        }
-        else
-        {
-            if (GUI.Button(buttonRect,"Stop Preview"))
-            {
-                SceneLoader.CloseUnactiveScenes();
-            }
+    //    }
+    //    else
+    //    {
+    //        if (GUI.Button(buttonRect,"Stop Preview"))
+    //        {
+    //            SceneLoader.CloseUnactiveScenes();
+    //        }
             
-        }
+    //    }
        
-        Handles.EndGUI();
+    //    Handles.EndGUI();
 
-    }
+    //}
 }

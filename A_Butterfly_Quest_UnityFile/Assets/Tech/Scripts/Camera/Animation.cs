f7be1ac7568a71d6cf709dfddd59b7d8d6d3d9c7
Robin Leplat <robin.leplat26@gmail.com>
2021-05-29 16:25:31 +0200

    AnimStart test

---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/Camera/Animation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public GameObject spawnPoint;
+
+    void LateUpdate()
+    {
+        transform.LookAt(spawnPoint.transform);
+    }
+}

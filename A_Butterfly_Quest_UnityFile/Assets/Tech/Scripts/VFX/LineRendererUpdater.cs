787ab54d1f1500809da52ab70434c982efbeee31
Assim <assimbuyumad59@gmail.com>
2021-06-01 03:15:45 +0200

    FX Tir setup et wip

---
+++ A_Butterfly_Quest_UnityFile/Assets/Tech/Scripts/VFX/LineRendererUpdater.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererUpdater : MonoBehaviour
+{
+    float timer;
+    public float TimeToDissolve = 1;
+    float erosionAmount;
+    List<LineRenderer> m_Renderers = new List<LineRenderer>();
+    List<Material> m_Mats = new List<Material>();
+    private void Start()
+    {
+        m_Renderers.Add(GetComponent<LineRenderer>());
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            m_Renderers.Add(transform.GetChild(i).GetComponent<LineRenderer>());
+        }
+        for (int i = 0; i < m_Renderers.Count; i++)
+        {
+            m_Mats.Add(m_Renderers[i].sharedMaterial);
+        }
+    }
+
+    void Update()
+    {
+        if(timer < TimeToDissolve)
+        {
+            erosionAmount = timer / TimeToDissolve;
+            UpdateRenderers();
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    void UpdateRenderers()
+    {
+        for (int i = 0; i < m_Renderers.Count; i++)
+        {
+            //m_Renderers.
+            m_Renderers[i].material.SetFloat("_Erosion", erosionAmount);
+        }
+    }
+}

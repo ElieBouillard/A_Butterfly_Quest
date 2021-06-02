13cad57aed19a0fc95700de455a6858738e17cbe
Robin Leplat <robin.leplat26@gmail.com>
2021-06-02 23:37:18 +0200

    son ennemis

--- A_Butterfly_Quest_UnityFile/Assets/CameraCinematique.cs
+++ A_Butterfly_Quest_UnityFile/Assets/CameraCinematique.cs
@@ -8,15 +8,26 @@ public class CameraCinematique : MonoBehaviour
 
     public Animator animator;
     public bool isAnimationFinished = false;
+    bool check;
     private void Awake()
     {
         instance = this;
+    }
+    private void Start()
+    {
+        Character3D.m_instance.FreezeInput = true;
     }
     void Update()
     {
         if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
         {
             isAnimationFinished = true;
+            if (check)
+            {
+                Character3D.m_instance.FreezeInput = false;
+                check = false;
+            }
+
         }
     }
     
